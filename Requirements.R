d8dc1b8e7da27b7ea6e3e23548c23bb56d681021
beambrosio <beamb0694@gmail.com>
2017-10-26 16:35:58 -0200

    Update Requirements.R

--- Requirements.R
+++ Requirements.R
@@ -1,7 +1,7 @@
 # Requirements for R
 
 # list of necessary packages
-packagesList <- c("XML", "RCurl", "downloader", "R.utils", "cluster", "ggplot")
+packagesList <- c("XML", "RCurl", "downloader", "R.utils", "cluster", "ggplot");
 
 # checks if the packages are installed if not put the non-istalled in a vector
 toInstall <- packagesList[!(packagesList %in% installed.packages()[, "Package"])]
